1f85c4c08e1af6288a24522bca3958e2db7045f6
manuelmorlap-cpu <manuelmorlap@gmail.com>
2025-11-21 11:58:10 -0300

    Refactor Flask app for deployment and history management
    Cambié la forma de iniciar la aplicación, estaba hecho como para iniciar de forma local con visualstudio. Últimas lineas de codigo

--- app.py
+++ app.py
@@ -154,6 +154,12 @@ def process():
         except Exception:
             pass
 
+#Para hacer launch de forma local con visualstudiocode
+#if __name__ == "__main__":
+    #app.run(host="127.0.0.1", port=7860, debug=False)
+
+#Para hacer launch desde railway o un servidor
 if __name__ == "__main__":
-    # Si tu entorno bloquea localhost, cambiá host="0.0.0.0"
-    app.run(host="127.0.0.1", port=7860, debug=False)
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port)
+
